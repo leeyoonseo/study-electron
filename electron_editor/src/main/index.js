d96b94a0685c13d43cbd132758864c363f155b85
leeyoonseo <okayoon.lee@gmail.com>
2021-11-20 20:41:06 +0900

    메뉴 로직 추가

--- electron_editor/src/main/index.js
+++ electron_editor/src/main/index.js
@@ -1,11 +1,17 @@
 import { app } from 'electron';
-// import creatMainWindow from './creatMainWindow';
 import createMainWindow from './createMainWindow';
+import setAppMenu from './setAppMenu';
 
 let mainWindow = null;
 
 app.on('ready', () => {
   mainWindow = createMainWindow();
+  setAppMenu({
+    openFile,
+    saveFile,
+    saveAsNewFile,
+    exportPDF
+  })
 });
 
 app.on('window-all-closed', () => {
@@ -19,3 +25,19 @@ app.on('activate', (_, hasVisibleWindow) => {
     mainWindow = createMainWindow();
   }
 });
+
+function openFile() {
+  console.log('openFile');
+}
+
+function saveFile() {
+  console.log('saveFile');
+}
+
+function saveAsNewFile() {
+  console.log('saveAsNewFile');
+}
+
+function exportPDF() {
+  console.log('exportPDF');
+}
---
+++ electron_editor/src/main/setAppMenu.js
@@ -0,0 +1,94 @@
+import { app, Menu, BrowserWindow } from 'electron';
+
+function setAppMenu(options) {
+  const template = [
+    {
+      label: "File",
+      submenu: [
+        { 
+          label: 'Open', 
+          accelerator: 'CmdOrCtrl+O', 
+          click: () => {
+            options.openFile();
+          }
+        },
+        {
+          label: 'Save', 
+          accelerator: 'CmdOrCtrl+S', 
+          click: () => {
+            options.saveFile();
+          }
+        },
+        {
+          label: 'Save As...', 
+          click: () => {
+            options.saveAsNewFile();
+          }
+        },
+        {
+          label: 'Export PDF', 
+          click: () => {
+            options.exportPDF();
+          }
+        }
+      ]
+    }, 
+    {
+      label: "Edit",
+      submenu: [
+        {
+          label: 'Copy', 
+          accelerator: 'CmdOrCtrl+C', 
+          role: 'copy',
+        },
+        {
+          label: 'Paste', 
+          accelerator: 'CmdOrCtrl+V', 
+          role: 'paste',
+        },
+        {
+          label: 'Cut', 
+          accelerator: 'CmdOrCtrl+X', 
+          role: 'cut',
+        },
+        {
+          label: 'Select All', 
+          accelerator: 'CmdOrCtrl+A', 
+          role: 'selectall',
+        },
+      ]  
+    },
+    {
+      label: "View",
+      submenu: [
+        {
+          label: 'Toggle DevTools', 
+          accelerator: 'Alt+Command+I', 
+          click: () => {
+            BrowserWindow.getFocusedWindow().toggleDevTools();
+          }
+        },
+      ]  
+    },
+  ];
+
+  if (process.platform === 'darwin') {
+    template.unshift(
+      {
+        label: 'MarkdownEditor',
+        submenu: [
+          {
+            label: 'Quit',
+            accelerator: 'CmdOrCtrl+Q',
+            click: () => {
+              app.quit()
+            }
+          }
+        ]
+      }
+    );
+  }
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+}
+
+export default setAppMenu;
