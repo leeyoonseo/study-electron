fe0f0914c81db4346d557561f4b2bc5648b88eb9
leeyoonseo <okayoon.lee@gmail.com>
2021-11-22 17:07:53 +0900

    미디어 스트림 추출

--- electron_caturer/src/main/index.js
+++ electron_caturer/src/main/index.js
@@ -10,12 +10,12 @@ app.on('ready', () => {
   // });
   captureWindow = createCaptureWindow();
   
-  const display = screen.getAllDisplays()[0];
-  const { x, y, width, height } = display.bounds;
-  const win = new BrowserWindow({
-    frame: false,
-    transparent: true,
-    alwaysOnTop: true,
-    x, y, width, height
-  });
+  // const display = screen.getAllDisplays()[0];
+  // const { x, y, width, height } = display.bounds;
+  // const win = new BrowserWindow({
+  //   frame: false,
+  //   transparent: true,
+  //   alwaysOnTop: true,
+  //   x, y, width, height
+  // });
 });
--- electron_caturer/src/renderer/captureWindow.js
+++ electron_caturer/src/renderer/captureWindow.js
@@ -1,12 +1,51 @@
-import { desktopCapturer } from 'electron';
+import { desktopCapturer, screen } from 'electron';
 
-desktopCapturer.getSources({
-  types: ['screen']
-}, (error, sources) => {
-  sources.forEach(targetSource => {
-    console.log(targetSource);
-    const thumbnailImg = document.createElement('img');
-    thumbnailImg.src = targetSource.thumbnail.toDataURL();
-    document.querySelector('body').appendChild(thumbnailImg);
+function getDesktopVideoStream(sourceDisplay) {
+  return new Promise((resolve, reject) => {
+    desktopCapturer.getSources({
+      types: ['screen']
+    }, (error, sources) => {
+      if (error) {
+        return reject(error);
+      }
+
+      let targetSource;
+      if (sources.length === 1) {
+        targetSource = sources[0];
+      } else {
+        targetSource = sources.find(source => source.name === sourceDisplay.name);
+      }
+
+      if (!targetSource) {
+        return reject({ message: 'No available source' });
+      }
+
+      // 스트림 객체 얻기 
+      navigator.webkitGetUserMedia({
+        audio: false,
+        video: {
+          mandatory: {
+            chromeMediaSource: 'desktop',
+            chromeMediaSourceId: targetSource.id,
+            minWidth: 100,
+            minHeight: 100,
+            maxWidth: 4096,
+            maxHeight: 4096,
+          }
+        }
+      }, resolve, reject);
+    });
   });
+}
+
+const sourceDisplay = screen.getPrimaryDisplay();
+sourceDisplay.name = 'Screen 1';
+
+getDesktopVideoStream(sourceDisplay).then(stream => {
+  const videoElement = document.createElement('video');
+  // 추출한 스트림을 객체 URL로 변환
+  videoElement.src = URL.createObjectURL(stream);
+  videoElement.play();
+  document.querySelector('body').appendChild(videoElement);
 });
+
