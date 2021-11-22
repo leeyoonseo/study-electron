3adc3113a83762006272b71e1ac62e525c898651
leeyoonseo <okayoon.lee@gmail.com>
2021-11-22 15:54:42 +0900

    바벨, 웹팩 설치 및 설정

--- electron_caturer/src/main/index.js
+++ electron_caturer/src/main/index.js
@@ -1,6 +1,12 @@
-import { app, screen } from 'electron';
+import { app, screen, BrowserWindow } from 'electron';
 
 app.on('ready', () => {
   const display = screen.getAllDisplays()[0];
-  console.log(display);
+  const { x, y, width, height } = display.bounds;
+  const win = new BrowserWindow({
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    x, y, width, height
+  });
 });
