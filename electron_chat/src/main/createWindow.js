d8195d60d640f2421af1567d21ee22134ca66fe6
leeyoonseo <okayoon.lee@gmail.com>
2021-11-23 18:29:00 +0900

    배포 작업 중

--- electron_chat/src/main/createWindow.js
+++ electron_chat/src/main/createWindow.js
@@ -3,7 +3,7 @@ import { BrowserWindow } from 'electron';
 let win;
 const createWindow = () => {
   win = new BrowserWindow();
-  win.loadURL(`file://${__dirname}/../../index.html`);
+  win.loadURL(`file://${__dirname}/../../prod_resources/index.html`);
   win.on('close', () => {
     win = null;
   });
