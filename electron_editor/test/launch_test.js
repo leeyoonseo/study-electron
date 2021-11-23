cbec375dfc14acb6fdf3bb1e0c74253f62edec35
leeyoonseo <okayoon.lee@gmail.com>
2021-11-23 15:51:44 +0900

    테스트 환경 준비

--- electron_editor/test/launch_test.js
+++ electron_editor/test/launch_test.js
@@ -1,22 +1,27 @@
-// 1. app 실행
-// 2. 출력 윈도우 수 세기
-// 3. 한개이면 테스트 성공
-
 const Application = require('spectron').Application;
 const electron = require('electron');
 const path = require('path');
+const assert = require('assert');
+
 const app = new Application({
   path: electron,
   args: [path.join(__dirname, '..')]
 });
 
-app.start()
-  .then(() => app.client.getWindowCount())
-  .then(count => {
-    if (count === 1) {
-      console.log('success test');
-    } else {
-      console.log('fail test');
-    }
-    app.stop();
+// this.timeout을 위해 arrow function 사용x
+describe('애플리케이션 실행 테스트', function () {
+  this.timeout(10000); // 10초 넘으면 테스트 실패
+
+  beforeEach(function () {
+    return app.start();
   });
+
+  afterEach(function () {
+    return app.stop();
+  });
+
+  it('애플리케이션이 실행된 뒤 윈도 하나를 출력했습니다.', function () {
+    return app.client.getWindowCount()
+      .then(count => assert.equal(count, 1));
+  });
+});
